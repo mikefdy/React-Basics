75f234a4df086ee689745981b0860772aafaac7e
mikefdy <Ml.fidanyan@gmail.com>
2022-05-24 16:57:12 +0200

    Hello Class implementation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';    //import App from './App.js'  importing component
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Hello />        
   </React.StrictMode>
-);
+);   //Was changed to <Hello /> before was <App />  displaying own component
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
