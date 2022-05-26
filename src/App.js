572b662be3f277bdfe53b2e99efefd3b5e8793fe
mikefdy <Ml.fidanyan@gmail.com>
2022-05-26 17:28:55 +0200

    Add Scrolling window and general optimization

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
-import {robots} from './Robots.js'
 import CardList from './CardList.js';
 import Searchbox from './Searchbox.js'
 import './App.css'
+import Scroll from './Scroll.js'
 
 //global parent app reuniting each component
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      robot: robots,
+      robot: [],
       searchfield:''
     }
+  }
+
+  //updating state lifecycle, from empty array to asssigning robot:robots array
+  //fetch is a https request method
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {return response.json();})
+    .then(users =>{ this.setState({robot:users}) })
+   
   }
 
   //event handler function whenever someone types in the search bar, changes the search value for the cards
@@ -24,19 +33,25 @@ class App extends Component {
   }
 
 
-
-
+//className = insert tachyon attributes
   render(){
     const filterRobots = this.state.robot.filter(robots =>{ return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());}) //comparing name and displaying output
-    return(
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <Searchbox searchChange = {this.onSearchChange}/>
-        <CardList robot={filterRobots}/>       
-      </div>
-    );
+    if(this.state.robot.length === 0){
+      return <h1 className='tc'>Loading...</h1>
+    }else{
+      return(
+        <div className='tc'> 
+          <h1 className='f1'>RoboFriends</h1>
+          <Searchbox searchChange = {this.onSearchChange}/>
+           <Scroll>                                                                    
+          <CardList robot={filterRobots}/>  
+          </Scroll>     
+        </div>
+      );
+    }
     
   }//with robot = filterRobot, only display the right or requested robot
+   //Scrolling Tag adds scrolling windowto the cardslist 
 }
  
 export default App;
